6107d7b1c94fd04689d123ada29205d3f9f6604f
Rutvik Manvar <rutvikmanvar99@gmail.com>
2026-02-06 19:09:08 +0530

    implement jwt auth

---
+++ services/auth.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken')
+const secret = 'Rutvik.7118'
+
+function setUser(user){
+    return jwt.sign(
+        {
+            _id:user._id,
+            email:user.email,
+            role: user.role 
+        },
+        secret
+    );
+}
+
+function getUser(token){
+    if(!token) return null;
+    try{
+        return jwt.verify(token,secret )
+    }catch(e){
+        return null;
+    }
+}
+
+module.exports = {
+    setUser,
+    getUser
+}
